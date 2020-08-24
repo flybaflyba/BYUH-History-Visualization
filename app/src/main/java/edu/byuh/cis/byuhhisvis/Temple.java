d54fc6dc2548e7efbf5eb82ab4758fba5debb4ff
Litian Zhang <litian_zhang17@163.com>
2020-08-24 11:13:53 -1000

    Start with LDS Temples Visualization code

---
+++ app/src/main/java/edu/byuh/cis/byuhhisvis/Temple.java
@@ -0,0 +1,35 @@
+package edu.byuh.cis.byuhhisvis;
+
+import android.graphics.Bitmap;
+
+public class Temple {
+    Bitmap image;
+    Float size;
+    Float x;
+    Float y;
+    String role;
+    String link;
+
+    public Temple(Bitmap imageP, Float sizeP, Float xP, Float yP) {
+        image = imageP;
+        size = sizeP;
+        x = xP;
+        y = yP;
+        role = "";
+        link = "";
+    }
+
+    public void setRole(String r) {
+        role = r;
+    }
+
+    public void setLink(String l) {
+        link = l;
+    }
+
+    public void changeImage(Bitmap b) {
+        image = b;
+    }
+
+
+}
